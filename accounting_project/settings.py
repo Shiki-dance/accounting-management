442eb621bccd13a08234d8ef1e0902b9f358486b
Shiki-dance <takutos.no1@gmail.com>
2024-11-13 22:55:44 +0900

    Add settings.py

--- accounting_project/settings.py
+++ accounting_project/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Herokuで静的ファイルをまとめる場所
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
