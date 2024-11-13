7512c810289fcb24d7128075b5ada29cab2f306e
Shiki-dance <takutos.no1@gmail.com>
2024-11-14 00:27:42 +0900

    Add root view for the accounting app

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+# accounting/views.py
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Welcome to the Accounting App!")
+
 urlpatterns = [
+    path('', views.index, name='index'),  # ルートURLに対するビュー
     path('department-expenses/', views.department_expenses, name='department_expenses'),
     path('age-expenses/', views.age_expenses, name='age_expenses'),
     path('monthly-personal-expenses/', views.monthly_personal_expenses, name='monthly_personal_expenses'),
