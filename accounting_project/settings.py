8a68165519b420bdef1612876b1a63fecc22336e
Shiki-dance <takutos.no1@gmail.com>
2024-11-15 18:01:26 +0900

    Fix template directory path

--- accounting_project/settings.py
+++ accounting_project/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'accounting_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'accounting' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
