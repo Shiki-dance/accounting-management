e4b94507722fd7b4caaa44da16e58bda0d1681ad
Shiki-dance <takutos.no1@gmail.com>
2024-11-22 17:56:38 +0900

    Add create expense view and template

--- accounting/urls.py
+++ accounting/urls.py
@@ -9,6 +9,7 @@ from django.http import HttpResponse
 urlpatterns = [
     path('', views.index, name='index'),  # ルートURLに対するビュー
     path('create-expenses/', views.create_expenses, name='create_expenses'),
+     path('expense-list/', views.expense_list, name='expense_list'),  # expense_listのURLパターン
     path('department-expenses/', views.department_expenses, name='department_expenses'),
     path('age-expenses/', views.age_expenses, name='age_expenses'),
     path('monthly-personal-expenses/', views.monthly_personal_expenses, name='monthly_personal_expenses'),
