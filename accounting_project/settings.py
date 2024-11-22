1d3ce5e1c9c6e93e59c06b623375066a17dbaeda
Shiki-dance <takutos.no1@gmail.com>
2024-11-22 16:33:43 +0900

    Fixed template path

--- accounting_project/settings.py
+++ accounting_project/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'accounting_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'accounting' / 'templates'],
+        'DIRS': [
+             os.path.join(BASE_DIR, 'accounting/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
